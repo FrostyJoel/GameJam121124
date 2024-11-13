9186823711d5200878d14033bc54261c6eae831b
Martijn Tielemans <martijn.tielemans@live.nl>
2024-11-13 15:29:36 +0100

    Added variation switching for microgames
    - Doesn't pick at random, adds one to the picked microgames each time.

--- scripts/microgame_manager.gd
+++ scripts/microgame_manager.gd
@@ -1,18 +1,30 @@
 extends Node2D
 
 @export var microgames : Array[MicrogameData]
+
+var timesPicked : Array[int]
 var lastPickedMicrogame : int = 999
 var currentMicrogame : Node
 
 signal microGameWin
 signal microGameLose
 
+func _ready() -> void:
+	# Initialize picked array
+	timesPicked.resize(microgames.size())
+	timesPicked.fill(0)
+
 # Spawns a random microgame
 func SpawnMicrogame():
 	var index = GetRandomIndex()
-	var variationToSpawn = microgames[index].variations[0]
+	var variationIndex = GetVariationIndex(index)
+	var variationToSpawn = microgames[index].variations[variationIndex]
+	print(variationIndex)
 	currentMicrogame = variationToSpawn.instantiate()
 	add_child(currentMicrogame)
+	
+	# Add one to times picked
+	timesPicked[index] += 1
 	
 	# Connect to microgame ended signal
 	currentMicrogame.microgameEnded.connect(MicrogameOver)
@@ -29,6 +41,14 @@ func GetRandomIndex() -> int:
 	
 	lastPickedMicrogame = randomIndex # Set last picked index
 	return randomIndex
+
+func GetVariationIndex(microgameIndex : int) -> int:
+	var index : int = timesPicked[microgameIndex]
+	if index >= microgames[microgameIndex].variations.size():
+		timesPicked[microgameIndex] = 0
+		index = 0
+	return index
+
 
 # Call this function to end the current microgame in either a success or failure
 func MicrogameOver(win: bool) -> void:
