6b8510b08d4c1859206289a07db81e9ebcf42989
DaanEijkhout <daan221718@buas.nl>
2024-11-14 10:58:50 +0100

    Changed rock paper scissor logic to properly work with new images

--- scripts/rockpaperscissors.gd
+++ scripts/rockpaperscissors.gd
@@ -4,7 +4,7 @@ extends microgame_base
 @onready var player: Label = $Player
 @onready var timer: Timer = $Timer
 @onready var playersprite: Sprite2D = $Player/PlayerSprite
-@onready var opponent_sprite: Sprite2D = $Opponent/OpponentSprite
+@onready var opponentsprite: Sprite2D = $Opponent/OpponentSprite
 
 var rocktexture = preload("res://assets/images/Rock.png")
 var rockflippedtexture = preload("res://assets/images/RockFlipped.png")
@@ -12,7 +12,6 @@ var papertexture = preload("res://assets/images/Paper.png")
 var paperflippedtexture = preload("res://assets/images/PaperFlipped.png")
 var scissortexture = preload("res://assets/images/Scissor.png")
 var scissorflippedtexture = preload("res://assets/images/ScissorFlipped.png")
-var currenttexture
 
 
 
@@ -20,64 +19,53 @@ func _ready() -> void:
 	#Assign opponent's choice
 	var randomint = randi_range(0,2)
 	if randomint == 0:
-		opponent.text = "ROCK"
-		opponent_sprite.texture = rockflippedtexture
+		opponentsprite.texture = rockflippedtexture
 	elif randomint == 1:
-		opponent.text = "PAPER"
-		opponent_sprite.texture = paperflippedtexture
+		opponentsprite.texture = paperflippedtexture
 	elif randomint == 2:
-		opponent.text = "SCISSORS"
-		opponent_sprite.texture = scissorflippedtexture
+		opponentsprite.texture = scissorflippedtexture
 		
-	player.text = "ROCK"
 	playersprite.texture = rocktexture
-	currenttexture = rocktexture
 	timer.start()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	# Change answer with stick(left)
 	if Input.is_action_just_pressed("DirLeft") or Input.is_action_just_pressed("NegativeHorizontal"):
-		if player.text == "ROCK":
-			player.text = "SCISSORS"
+		if playersprite.texture == rocktexture:	
 			playersprite.texture = scissortexture
-		elif player.text == "PAPER":
-			player.text = "ROCK"
+		elif playersprite.texture == papertexture:
 			playersprite.texture = rocktexture
-		elif player.text == "SCISSORS":
-			player.text = "PAPER"
+		elif playersprite.texture == scissortexture:
 			playersprite.texture = papertexture
 			
 	# Change answer with stick(right)
 	if Input.is_action_just_pressed("DirRight") or Input.is_action_just_pressed("PostiveHorizontal"):
-		if player.text == "ROCK":
-			player.text = "PAPER"
+		if playersprite.texture == rocktexture:
 			playersprite.texture = papertexture
-		elif player.text == "PAPER":
-			player.text = "SCISSORS"
+		elif playersprite.texture == papertexture:
 			playersprite.texture = scissortexture
-		elif player.text == "SCISSORS":
-			player.text = "ROCK"
+		elif playersprite.texture == scissortexture:
 			playersprite.texture = rocktexture
 		
 	# Enter answer
 	if Input.is_action_just_pressed("Action1") or Input.is_action_just_pressed("Action2"):
-		if opponent.text == "ROCK":
-			if player.text == "PAPER":
+		if opponentsprite.texture == rockflippedtexture:
+			if playersprite.texture == papertexture:
 				print("YOU WIN")
 				microgameEnded.emit(true)
 			else:
 				print("YOU LOSE")
 				microgameEnded.emit(false)
-		if opponent.text == "PAPER":
-			if player.text == "SCISSORS":
+		if opponentsprite.texture == paperflippedtexture:
+			if playersprite.texture == scissortexture:
 				print("YOU WIN")	
 				microgameEnded.emit(true)
 			else:
 				print("YOU LOSE")
 				microgameEnded.emit(false)
-		if opponent.text == "SCISSORS":
-			if player.text == "ROCK":
+		if opponentsprite.texture == scissorflippedtexture:
+			if playersprite.texture == rocktexture:
 				print("YOU WIN")
 				microgameEnded.emit(true)
 			else:
