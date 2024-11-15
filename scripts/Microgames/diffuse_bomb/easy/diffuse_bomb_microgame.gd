5ac9da2dacf207f31e8b68b364a890b9e88be10d
Martijn Tielemans <martijn.tielemans@live.nl>
2024-11-15 13:53:55 +0100

    Fixed timer setting in bomb microgame

--- scripts/Microgames/diffuse_bomb/easy/diffuse_bomb_microgame.gd
+++ scripts/Microgames/diffuse_bomb/easy/diffuse_bomb_microgame.gd
@@ -32,14 +32,14 @@ func _ready() -> void:
 	$Timer.start()
 	
 	#Needed to find and enableTimer
-	var uiManager = get_tree().get_nodes_in_group("Ui")
+	var uiManager = get_tree().get_nodes_in_group("UiManager")
 	for node in uiManager:
 		# Check the node has a save function.
 		if !node.has_method("Enabletimer"):
 			print("persistent node '%s' is missing a Enabletimer() function, skipped" % node.name)
 			continue
 		
-		node.call("Enabletimer", $Timer.wait_time)
+		node.call("Enabletimer", $Timer)
 	
 	for n in AmountOfInputs:
 		var ActiveButtonFromEnum = randi_range(0, Input_Keys.size()-1)
