e5eec52269519816ad8acf9fa27cb43e9e17c2e9
FunkyRuben <Rubenvangijsel@gmail.com>
2024-11-15 14:02:38 +0100

    added timer to hotdog games

--- scenes/Microgames/hotdogcontest/hot_dog_eater.gd
+++ scenes/Microgames/hotdogcontest/hot_dog_eater.gd
@@ -9,13 +9,14 @@ const DogEaterNumber = 3
 func _ready() -> void:
 	$Timer.start()
 	#Needed to find and enableTimer
-	#var uiManager = get_tree().get_nodes_in_group("Ui")
-	#for node in uiManager:
-		## Check the node has a save function.
-		#if !node.has_method("Enabletimer"):
-			#print("persistent node '%s' is missing a Enabletimer() function, skipped" % node.name)
-			#continue
-		#node.call("Enabletimer",7)
+	var uiManager = get_tree().get_nodes_in_group("UiManager")
+	for node in uiManager:
+		# Check the node has a save function.
+		if !node.has_method("Enabletimer"):
+			print("persistent node '%s' is missing a Enabletimer() function, skipped" % node.name)
+			continue
+		node.call("Enabletimer",7)
+		$DogEaterNumber.text = str(DogEaterNumber)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scenes/Microgames/hotdogcontest/hot_dog_eater2.gd
+++ scenes/Microgames/hotdogcontest/hot_dog_eater2.gd
@@ -8,6 +8,14 @@ const DogEaterNumber = 6
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Timer.start()
+		#Needed to find and enableTimer
+	var uiManager = get_tree().get_nodes_in_group("UiManager")
+	for node in uiManager:
+		# Check the node has a save function.
+		if !node.has_method("Enabletimer"):
+			print("persistent node '%s' is missing a Enabletimer() function, skipped" % node.name)
+			continue
+		node.call("Enabletimer",7)
 	$DogEaterNumber.text = str(DogEaterNumber)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
