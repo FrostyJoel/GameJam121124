16ba7facb633d0cfaa38a82e8c374de6299a8668
DaanEijkhout <daan221718@buas.nl>
2024-11-15 10:58:27 +0100

    FIX doodle jump fail state not working

--- scripts/DoodleJump/doodle_jump.gd
+++ scripts/DoodleJump/doodle_jump.gd
@@ -2,7 +2,7 @@ extends microgame_base
 var state = false
 
 func _on_game_timer_timeout() -> void:
-	$GameTimer.start()
+	$EndTimer.start()
 	$Player.gameended = true
 
 func _on_end_timer_timeout() -> void:
